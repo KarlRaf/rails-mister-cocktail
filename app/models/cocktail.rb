87a0320109a2128d19d7fe55fb25ec00f1a3843f
Karl Raf <karlforbusiness@gmail.com>
2018-05-18 12:59:44 +0200

    validates photo

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,5 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, allow_blank: false, uniqueness: true
+  validates :photo, presence: true
 end
